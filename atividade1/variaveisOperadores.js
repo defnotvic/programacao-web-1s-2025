f69115bf1f87d08621cfbe49836e0015fc92e4aa
Paulo Haddad Boëchat <117122801+defnotvic@users.noreply.github.com>
2025-03-06 15:08:49 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,14 @@
+let num_A = 10;
+let num_B = 5;
+
+let resultado_adicao = num_A + num_B;
+console.log(`Resultado da adição entre ${num_A} e ${num_B} é ${resultado_adicao}`);
+
+let resultado_subtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${resultado_subtracao}`);
+
+let resultado_multiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${resultado_multiplicacao}`);
+
+let resultado_divisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${resultado_divisao}`);
